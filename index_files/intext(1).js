c21fe9dec1691845b608759f7b60133bdf066ebb
Jude Naveen Raj <judenaveenraj@yahoo.co.in>
2014-05-06 00:46:32 +0530

    first commit

---
+++ index_files/intext(1).js
@@ -0,0 +1,9 @@
+ var $adtextsup = (function (window) 
+  { 
+   var appNamespace = '$adtextsup'; 
+   var pub={};
+   var version = ''; 
+   var opts = {  s1: '', s3: '', cookies: 'branduid=fe989fc3225e95b49d73b2e70954ee5c|1398338080; vc=24'};
+   pub.config= function () { return('<sbinfo>?</sbinfo>');}
+   return(pub);
+ }(window)); try { $adtext.analyzePage();} catch(e){}
