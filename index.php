9d39c1ff9729f5d85b9d7aa94f49b98de7f6c4b0
Sriram Sundarraj <sriram.s.1994@gmail.com>
2014-05-19 01:35:00 +0530

    font change

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ if(!defined('__PRAGYAN_CMS'))
 	?>
 
 	<link href='http://fonts.googleapis.com/css?family=Quicksand:400,700' rel='stylesheet' type='text/css' />
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
 	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 
 
